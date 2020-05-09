7915ffef929ae22be5b93334d5d44aeafc2249f1
Laineyzhang55 <lainey_zhang47@163.com>
2020-05-09 17:17:52 +0800

    代码模块化拆分

--- src/index.js
+++ src/index.js
@@ -1,54 +1,9 @@
-function createElement(tag, attrs, ...children) {
-  return{
-    tag,
-    attrs,
-    children
-  }   
-}
+import React from './lib/react';
+import ReactDOM from './lib/react-dom';
 
-const React = {
-  createElement
-};
 
-function setAttribute(node, attrs) {
-  if(!attrs) return
-
-  for(let key in attrs) {
-    if(key.startsWith('on')){
-      node[key.toLocaleLowerCase()] = attrs[key];
-    }else if(key === 'style'){
-      Object.assign(node.style, attrs[key]);
-    } else {
-      node[key] = attrs[key];
-    }
-  }
-}
-
-function render(vdom, container) {
-  let node;
-  if(typeof vdom === 'string') {
-  node = document.createTextNode(vdom);
-  }
-  if(typeof vdom === 'object') {
-  node = document.createElement(vdom.tag);
-  setAttribute(node, vdom.attrs);
-  vdom.children.forEach(childVdom => render(childVdom, node));
-  }
-  container.appendChild(node);
-} 
-
-const ReactDOM = {
-  render(vdom, container) {
-    container.inerHTML=''
-    render(vdom,container)
-  }
-}
-
-let div = (
-  <div className="text" style={ {color:"red"} } Onclick={(()=>console.log('click me'))}> 
+ReactDOM.render((
+  <div className="text" style={ {color:"blue"} }> 
     <p>hello <span>bigger </span>word</p>
   </div>
-)
-console.log(div);
-
-ReactDOM.render(div, document.body)
+), document.body)
