f370b51775d5bfac9cb67e4a0f1f8feb2a8bb57a
Laineyzhang55 <lainey_zhang47@163.com>
2020-05-09 16:18:54 +0800

    revise index.js for render dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
+function createElement(tag, attrs, ...children) {
+  return{
+    tag,
+    attrs,
+    children
+  }   
+}
+
 const React = {
-  createElement(...args) {
-    console.log(args)
-  }
+  createElement
 };
 
-let div = <div>hello </div>;
+
+function render(vdom, container) {
+  let node;
+  if(typeof vdom === 'string') {
+  node = document.createTextNode(vdom);
+  }
+  if(typeof vdom === 'object') {
+  node = document.createElement(vdom.tag);
+  vdom.children.forEach(childVdom => render(childVdom, node));
+  }
+  container.appendChild(node);
+} 
+
+const ReactDOM = {
+  render(vdom, container) {
+    container.inerHTML=''
+    render(vdom,container)
+  }
+}
+
+let div = (
+  <div> 
+    <p>hello <span>biger </span>word</p>
+    
+  </div>
+)
 console.log(div);
+
+ReactDOM.render(div, document.body)
