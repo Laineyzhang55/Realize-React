873c965bdb2e62b616caad320eedb3300c681f49
Laineyzhang55 <lainey_zhang47@163.com>
2020-05-09 20:01:08 +0800

    create clsaa

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from './lib/react';
 import ReactDOM from './lib/react-dom';
 
+class Article extends React.Component {
 
-ReactDOM.render((
-  <div className="text" style={ {color:"blue"} }> 
-    <p>hello <span>bigger </span>word</p>
-  </div>
-), document.body)
+  render() {
+    return <p>hello { this.props.title }</p>;
+  }
+}
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '测试1'
+    };
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>App</h1>
+        <Article title={ this.state.title }></Article>
+        <div> <button onClick={ this.clickMe.bind(this) }> Click Me</button> </div>
+      </div>
+    )
+  }
+
+  clickMe() {
+    this.setState({title: '测试2'});
+  }
+}
+
+ReactDOM.render(<App />, document.body)
